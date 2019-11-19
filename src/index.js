730651e556a2e3d69511f0ce852f1ff3cdea81b8
Jonatas Sales <jonatas.2.sales.external@bt.com>
2019-11-19 16:41:34 -0300

    socket starting, prob change to service or global state

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import { apiRouter } from './routes'
-
-//  import auth from './middlewares/authentication'
+import socket from 'socket.io'
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
-// app.use(auth)
 app.use(apiRouter())
 
 const port = 5000
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`${port}`)
+})
+
+var io = socket(server)
+
+io.on('connection', (socket) => {
+  console.log('made connection')
+  setTimeout(() => io.emit('FromAPI', 'bobo'), 3000)
 })
--- src/services/authServices.js
+++ src/services/authServices.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 import { SECRET } from '../utils/secretKey'
 
 function generateToken (username) {
-  return jwt.sign({ username }, SECRET, { algorithm: 'HS256', expiresIn: '1h' })
+  return jwt.sign({ username }, SECRET, { algorithm: 'HS256', expiresIn: '5h' })
 }
 
 export { generateToken }
